7ef042dee4cd7818c1e20f59e65a4006048b3c54
Michael Koloberdin <koloberdin@gmail.com>
2017-08-15 04:37:13 +0300

    Looks like we need to find/specify the config file path after all
    + Fix spec/tests
    + More info on .hlint.yaml files: https://neilmitchell.blogspot.com/2017/04/hlint-20-with-yaml-configuration.html

--- spec/linter-hlint-spec.js
+++ spec/linter-hlint-spec.js
@@ -28,13 +28,6 @@ describe('The hlint provider for Linter', () => {
         expect(messages[0].html).not.toBeDefined();
         expect(messages[0].filePath).toBe(concatPath);
         expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
-
-        expect(messages[1].type).toBe('Info');
-        expect(messages[1].severity).toBe('info');
-        expect(messages[1].text).toBe('Use fmap: map ==> fmap');
-        expect(messages[1].html).not.toBeDefined();
-        expect(messages[1].filePath).toBe(concatPath);
-        expect(messages[1].range).toEqual([[0, 17], [0, 20]]);
       }),
     );
   });
@@ -50,8 +43,8 @@ describe('The hlint provider for Linter', () => {
   it('works with configuration files', () => {
     waitsForPromise(() =>
       atom.workspace.open(configPath).then(editor => lint(editor)).then((messages) => {
-        // The config should make it not know about the fmap suggestion
-        expect(messages.length).toBe(1);
+        // The config should enable an fmap suggestion
+        expect(messages.length).toBe(2);
 
         expect(messages[0].type).toBe('Warning');
         expect(messages[0].severity).toBe('warning');
@@ -59,6 +52,13 @@ describe('The hlint provider for Linter', () => {
         expect(messages[0].html).not.toBeDefined();
         expect(messages[0].filePath).toBe(configPath);
         expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
+
+        expect(messages[1].type).toBe('Warning');
+        expect(messages[1].severity).toBe('warning');
+        expect(messages[1].text).toBe('Use fmap: map ==> fmap');
+        expect(messages[1].html).not.toBeDefined();
+        expect(messages[1].filePath).toBe(configPath);
+        expect(messages[1].range).toEqual([[0, 17], [0, 20]]);
       }),
     );
   });
