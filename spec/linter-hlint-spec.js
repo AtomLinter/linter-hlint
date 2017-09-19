93b026a7ad4f67634a9675206ed4805818a192ec
Landon Abney <Landonabney@gmail.com>
2017-09-19 14:17:00 -0700

    Asyncify the specs
    Bring in `jasmine-fix` to allow the use of `async`/`await` in the specs
    and refactor them to take advantage of this.

--- spec/linter-hlint-spec.js
+++ spec/linter-hlint-spec.js
@@ -1,61 +1,60 @@
 'use babel';
 
+// eslint-disable-next-line no-unused-vars
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import * as path from 'path';
+
+const { lint } = require('../lib/init.js').provideLinter();
 
 const concatPath = path.join(__dirname, 'fixtures', 'concat.hs');
 const validPath = path.join(__dirname, 'fixtures', 'valid.hs');
 const configPath = path.join(__dirname, 'fixtures', 'config', 'concat.hs');
 
 describe('The hlint provider for Linter', () => {
-  const { lint } = require('../lib/init.js').provideLinter();
-
-  beforeEach(() => {
+  beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
-    waitsForPromise(() =>
-      Promise.all([
-        atom.packages.activatePackage('linter-hlint'),
-        atom.packages.activatePackage('language-haskell'),
-      ]));
+    await atom.packages.activatePackage('linter-hlint');
+    await atom.packages.activatePackage('language-haskell');
   });
 
-  it('works with default lints', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(concatPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages[0].type).toBe('Warning');
-        expect(messages[0].severity).toBe('warning');
-        expect(messages[0].text).toBe('Use concatMap: concat (map op xs) ==> concatMap op xs');
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].filePath).toBe(concatPath);
-        expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
-      }));
+  it('works with default lints', async () => {
+    const editor = await atom.workspace.open(concatPath);
+    const messages = await lint(editor);
+
+    expect(messages[0].type).toBe('Warning');
+    expect(messages[0].severity).toBe('warning');
+    expect(messages[0].text).toBe('Use concatMap: concat (map op xs) ==> concatMap op xs');
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].filePath).toBe(concatPath);
+    expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
   });
 
-  it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(validPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(0);
-      }));
+  it('finds nothing wrong with a valid file', async () => {
+    const editor = await atom.workspace.open(validPath);
+    const messages = await lint(editor);
+
+    expect(messages.length).toBe(0);
   });
 
-  it('works with configuration files', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(configPath).then(editor => lint(editor)).then((messages) => {
-        // The config should enable an fmap suggestion
-        expect(messages.length).toBe(2);
+  it('works with configuration files', async () => {
+    const editor = await atom.workspace.open(configPath);
+    const messages = await lint(editor);
 
-        expect(messages[0].type).toBe('Warning');
-        expect(messages[0].severity).toBe('warning');
-        expect(messages[0].text).toBe('Use concatMap: concat (map op xs) ==> concatMap op xs');
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].filePath).toBe(configPath);
-        expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
+    // The config should enable an fmap suggestion
+    expect(messages.length).toBe(2);
 
-        expect(messages[1].type).toBe('Warning');
-        expect(messages[1].severity).toBe('warning');
-        expect(messages[1].text).toBe('Use fmap: map ==> fmap');
-        expect(messages[1].html).not.toBeDefined();
-        expect(messages[1].filePath).toBe(configPath);
-        expect(messages[1].range).toEqual([[0, 17], [0, 20]]);
-      }));
+    expect(messages[0].type).toBe('Warning');
+    expect(messages[0].severity).toBe('warning');
+    expect(messages[0].text).toBe('Use concatMap: concat (map op xs) ==> concatMap op xs');
+    expect(messages[0].html).not.toBeDefined();
+    expect(messages[0].filePath).toBe(configPath);
+    expect(messages[0].range).toEqual([[0, 9], [0, 27]]);
+
+    expect(messages[1].type).toBe('Warning');
+    expect(messages[1].severity).toBe('warning');
+    expect(messages[1].text).toBe('Use fmap: map ==> fmap');
+    expect(messages[1].html).not.toBeDefined();
+    expect(messages[1].filePath).toBe(configPath);
+    expect(messages[1].range).toEqual([[0, 17], [0, 20]]);
   });
 });
